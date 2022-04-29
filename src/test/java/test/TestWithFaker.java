62009f10651462e4e56595ed30e0d7cc5b9ef42f
Margo4490 <95082876+Margo4490@users.noreply.github.com>
2022-04-30 00:39:01 +0300

    happy test

--- src/test/java/test/TestWithFaker.java
+++ src/test/java/test/TestWithFaker.java
@@ -1,4 +1,4 @@
-package utils;
+package test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
@@ -7,6 +7,7 @@ import com.github.javafaker.Faker;
 import entities.RegistrationInfo;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import utils.DataGenerator;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -14,45 +15,42 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static java.awt.SystemColor.info;
+import static utils.DataGenerator.generateDate;
 
 public class TestWithFaker {
-    private static Faker faker;
-    RegistrationInfo info = DataGenerator
-            .Registration
-            .generateInfo("ru");
 
-    LocalDate localDate = LocalDate.now().plusDays(3);
-    DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String strData = localDate.format(data);
-
-    LocalDate localDate1 = LocalDate.now().plusDays(6);
-    DateTimeFormatter data1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String strData1 = localDate1.format(data1);
 
     @Test
     void happyPathAfterChangeDate() {
 
         Configuration.holdBrowserOpen = true;
         Selenide.open("http://localhost:9999");
-        $("[data-test-id='city'] input").val(info.getCity());
+        $("[data-test-id='city'] input").val(DataGenerator.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id='name'] input").val(info.getName());
-        $("[data-test-id='phone'] input").val(info.getPhone());
+
+        String scheduledDate = generateDate(3);
+
+        $("[data-test-id='date'] input").val(scheduledDate);
+        $("[data-test-id='name'] input").val(DataGenerator.getName());
+        $("[data-test-id='phone'] input").val(DataGenerator.getNumberPhone());
         $("[data-test-id='agreement']").click();
         $("div .button").click();
         $("[data-test-id='success-notification'] .notification__title").should(visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content")
-                .should(Condition.text("Встреча успешно запланирована на " + strData), Duration.ofSeconds(15));
+                .should(Condition.text("Встреча успешно запланирована на " + scheduledDate), Duration.ofSeconds(15));
 
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData1);
+
+        String rescheduledDate = generateDate(6);
+
+        $("[data-test-id='date'] input").val(rescheduledDate);
         $("div .button").click();
         $("[data-test-id='replan-notification'] .notification__content")
                 .should(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15));
         $("[data-test-id='replan-notification'] .button").click();
         $("[data-test-id='success-notification'] .notification__content")
-                .should(Condition.text("Встреча успешно запланирована на " + strData1), Duration.ofSeconds(15));
+                .should(Condition.text("Встреча успешно запланирована на " + rescheduledDate), Duration.ofSeconds(15));
 
     }
 
--- src/test/java/utils/DataGenerator.java
+++ src/test/java/utils/DataGenerator.java
@@ -2,19 +2,52 @@ package utils;
 
 import com.github.javafaker.Faker;
 import entities.RegistrationInfo;
+import lombok.Data;
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+@Data
+@Getter
 @UtilityClass
+
 public class DataGenerator {
+    private static final Faker faker = new Faker(new Locale("ru"));
 
-    @UtilityClass
-    public static class Registration{
-        public static RegistrationInfo generateInfo(String locale){
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationInfo(faker.address().cityName(),faker.name().fullName(),faker.phoneNumber().phoneNumber());
 
+    public static class RegistrationInfo {
+
+        public RegistrationInfo(String city, String name, String numberPhone) {
         }
+    }
+
+    public static String generateDate(int days) {
+
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String getCity() {
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String getName() {
+        String name = faker.name().name();
+        return name;
+    }
+
+    public static String getNumberPhone() {
+        String phoneNumber = faker.phoneNumber().phoneNumber();
+        return phoneNumber;
+
+    }
+
+    public static RegistrationInfo getUser() {
+        RegistrationInfo user = new RegistrationInfo(getCity(), getName(), getNumberPhone());
+        return user;
     }
 }
